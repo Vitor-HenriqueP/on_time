45088ec53a1a48e6cd96d2399fe6b86075ac36d2
vinicius <vinianderle00@gmail.com>
2024-11-05 13:57:06 -0300

    feature register download

--- lib/comprove.dart
+++ lib/comprove.dart
@@ -178,7 +178,7 @@ class ComproveScreen extends StatelessWidget {
       final bytes = await pdf.save();
       final blob = html.Blob([bytes], 'application/pdf');
       final url = html.Url.createObjectUrlFromBlob(blob);
-      final anchor = html.AnchorElement(href: url)
+      final anchor = html.AnchorElement(href: ur
         ..setAttribute('download', 'comprovante.pdf')
         ..click();
       html.Url.revokeObjectUrl(url);
